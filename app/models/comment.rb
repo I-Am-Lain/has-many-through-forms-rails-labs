bdd8166dded03c98f70859ff6a466838a9a104cb
I-Am-Lain <vetter.william@gmail.com>
2020-10-16 05:10:33 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attrs)
+    user_attrs.values.each do |v|
+      if v != ""
+        user = User.find_or_create_by(username: v)
+        self.user = user
+      end
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,40 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  #accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attrs)
+    category_attrs.values.each do |v|
+      if v[:name] != ""
+        cat = Category.find_or_create_by(v)
+        self.categories << cat
+      end
+    end
+  end
+
+  # def users_attributes=(user_attrs)
+  #   user_attrs.values.each do |v|
+  #     if v[:username] != ""
+  #       user = User.find_or_create_by(v)
+  #       self.users << user
+  #     end
+  #   end
+  # end
+
+  # def comments_attributes=(comment_attrs)
+  #   comment_attrs.values.each do |v|
+  #     if v[:content] != ""
+  #       comm = Comment.find_or_create_by(v)
+  #       self.comments << comm
+  #     end
+  #   end
+  # end
+
+  def unique_visitors
+    self.comments.map do |c|
+      c.user
+    end.uniq
+  end
 
 
 end
